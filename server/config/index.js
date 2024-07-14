fa9e0364452fe9d983c13e66a7a6093a786f9a23
JSLSasiRekha <105384982+JSLSasiRekha@users.noreply.github.com>
2024-07-14 15:34:54 +0530

    added config

---
+++ server/config/index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const apiKey = process.env.FirebaseapiKey;
+const authDomain = process.env.FirebaseauthDomain;
+const projectId = process.env.FirebaseprojectId;
+const storageBucket = process.env.FirebasestorageBucket;
+const messagingSenderId = process.env.FirebasemessagingSenderId;
+const appId = process.env.FirebaseappId;
+const measurementId = process.env.FirebasemeasurementId;
+
+const serviceAccount = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key.replace(/\\n/g, "\n"),
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain,
+};
+
+module.exports = {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+  serviceAccount,
+};
