1d8ec818924027585ba65ef4ce8dd7f115b20472
JSLSasiRekha <105384982+JSLSasiRekha@users.noreply.github.com>
2024-07-18 23:20:06 +0530

    modified

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,3 +1,3 @@
 
-// export const url =`${import.meta.env.VITE_BACKEND_URL}`;
-export const url ="http://localhost:8000";
+export const url =`${import.meta.env.VITE_BACKEND_URL}`;
+// export const url ="http://localhost:8000";
